0ec655ae054918a3c069666570bc725ac6b84c0b
Miei <miei1@outlook.com>
2016-05-15 15:09:50 +0100

    improved url matching, add option for url matching

--- addons/classicchat/classicchat.lua
+++ addons/classicchat/classicchat.lua
@@ -4,6 +4,7 @@ local settings = {
 	hideSystemName = true;			-- hide the sender name for system messages
 	urlClickWarning = true;			-- warning before opening a web url
 	timeStamp = true;				-- show timestamps
+	urlMatching = true;				-- do you want to enable URL links in chat?
 };
 
 settings.whisperSound = {
@@ -52,6 +53,29 @@ settings.itemColors = setmetatable({
 
 local classicChat = _G["ADDONS"]["CLASSICCHAT"];
 local myFamilyName = GETMYFAMILYNAME();
+
+
+local domains = [[.ac.ad.ae.aero.af.ag.ai.al.am.an.ao.aq.ar.arpa.as.asia.at.au
+	.aw.ax.az.ba.bb.bd.be.bf.bg.bh.bi.biz.bj.bm.bn.bo.br.bs.bt.bv.bw.by.bz.ca
+	.cat.cc.cd.cf.cg.ch.ci.ck.cl.cm.cn.co.com.coop.cr.cs.cu.cv.cx.cy.cz.dd.de
+	.dj.dk.dm.do.dz.ec.edu.ee.eg.eh.er.es.et.eu.fi.firm.fj.fk.fm.fo.fr.fx.ga
+	.gb.gd.ge.gf.gh.gi.gl.gm.gn.gov.gp.gq.gr.gs.gt.gu.gw.gy.hk.hm.hn.hr.ht.hu
+	.id.ie.il.im.in.info.int.io.iq.ir.is.it.je.jm.jo.jobs.jp.ke.kg.kh.ki.km.kn
+	.kp.kr.kw.ky.kz.la.lb.lc.li.lk.lr.ls.lt.lu.lv.ly.ma.mc.md.me.mg.mh.mil.mk
+	.ml.mm.mn.mo.mobi.mp.mq.mr.ms.mt.mu.museum.mv.mw.mx.my.mz.na.name.nato.nc
+	.ne.net.nf.ng.ni.nl.no.nom.np.nr.nt.nu.nz.om.org.pa.pe.pf.pg.ph.pk.pl.pm
+	.pn.post.pr.pro.ps.pt.pw.py.qa.re.ro.ru.rw.sa.sb.sc.sd.se.sg.sh.si.sj.sk
+	.sl.sm.sn.so.sr.ss.st.store.su.sv.sy.sz.tc.td.tel.tf.tg.th.tj.tk.tl.tm.tn
+	.to.tp.tr.travel.tt.tv.tw.tz.ua.ug.uk.um.us.uy.va.vc.ve.vg.vi.vn.vu.web.wf
+	.ws.xxx.ye.yt.yu.za.zm.zr.zw]]
+
+local tlds = {}
+for tld in domains:gmatch'%w+' do
+	tlds[tld] = true
+end
+local function max4(a,b,c,d) return math.max(a+0, b+0, c+0, d+0) end
+local protocols = {[''] = 0, ['http://'] = 0, ['https://'] = 0, ['ftp://'] = 0}
+
 
 function CLASSICCHAT_DRAW_CHAT_MSG(groupBoxName, size, startIndex, frameName)
 
@@ -195,18 +219,12 @@ function CLASSICCHAT_DRAW_CHAT_MSG(groupBoxName, size, startIndex, frameName)
 			messageText =  string.format("%s%s%s%s%s:{/}%s %s", tagStyleString, settings.formatting.indentation, timeStamp, channelTag, nameTag, styleString, messageText);
 		end
 
+		if settings.urlMatching == true then
+			messageText = classicChat.processUrls(messageText);
+		end
 
 		messageText = classicChat.escape(messageText);
 		messageText = messageText .. " ";
-
-		for link in messageText:gmatch("(https?:.-)[{ ]") do
-			local length = 25;
-			if #classicChat.unescape(link) > length then
-				messageText = messageText:gsub(link, string.format("{a SLL %s}{#%s}%s!@#DOT#@!!@#DOT#@!!@#DOT#@!{/}{/}{/}", link, settings.chatColors.Link, classicChat.escape(classicChat.unescape(link):sub(1, length))));
-			else
-				messageText = messageText:gsub(link, string.format("{a SLL %s}{#%s}%s{/}{/}{/}", link, settings.chatColors.Link, link));
-			end
-		end
 
 		-- refresh style after {/} but not {/}{
 		messageText = classicChat.instertText(messageText, "{/}[^{]", "{/}", styleString);
@@ -388,6 +406,83 @@ function SLL(text, warned)
 	end
 end
 
+local domains = [[.ac.ad.ae.aero.af.ag.ai.al.am.an.ao.aq.ar.arpa.as.asia.at.au
+	.aw.ax.az.ba.bb.bd.be.bf.bg.bh.bi.biz.bj.bm.bn.bo.br.bs.bt.bv.bw.by.bz.ca
+	.cat.cc.cd.cf.cg.ch.ci.ck.cl.cm.cn.co.com.coop.cr.cs.cu.cv.cx.cy.cz.dd.de
+	.dj.dk.dm.do.dz.ec.edu.ee.eg.eh.er.es.et.eu.fi.firm.fj.fk.fm.fo.fr.fx.ga
+	.gb.gd.ge.gf.gh.gi.gl.gm.gn.gov.gp.gq.gr.gs.gt.gu.gw.gy.hk.hm.hn.hr.ht.hu
+	.id.ie.il.im.in.info.int.io.iq.ir.is.it.je.jm.jo.jobs.jp.ke.kg.kh.ki.km.kn
+	.kp.kr.kw.ky.kz.la.lb.lc.li.lk.lr.ls.lt.lu.lv.ly.ma.mc.md.me.mg.mh.mil.mk
+	.ml.mm.mn.mo.mobi.mp.mq.mr.ms.mt.mu.museum.mv.mw.mx.my.mz.na.name.nato.nc
+	.ne.net.nf.ng.ni.nl.no.nom.np.nr.nt.nu.nz.om.org.pa.pe.pf.pg.ph.pk.pl.pm
+	.pn.post.pr.pro.ps.pt.pw.py.qa.re.ro.ru.rw.sa.sb.sc.sd.se.sg.sh.si.sj.sk
+	.sl.sm.sn.so.sr.ss.st.store.su.sv.sy.sz.tc.td.tel.tf.tg.th.tj.tk.tl.tm.tn
+	.to.tp.tr.travel.tt.tv.tw.tz.ua.ug.uk.um.us.uy.va.vc.ve.vg.vi.vn.vu.web.wf
+	.ws.xxx.ye.yt.yu.za.zm.zr.zw]]
+
+function classicChat.processUrls(text)
+	local minLength = 8;
+	local tlds = {}
+	for tld in domains:gmatch'%w+' do
+		tlds[tld] = true
+	end
+	local function max4(a,b,c,d) return math.max(a+0, b+0, c+0, d+0) end
+	local protocols = {[''] = 0, ['http://'] = 0, ['https://'] = 0, ['ftp://'] = 0}
+	local finished = {}
+	local textNew = text;
+	for pos_start, url, prot, subd, tld, colon, port, slash, path in
+		text:gmatch'()(([%w_.~!*:@&+$/?%%#-]-)(%w[-.%w]*%.)(%w+)(:?)(%d*)(/?)([%w_.~!*:@&+$/?%%#=-]*))'
+	do
+		if protocols[prot:lower()] == (1 - #slash) * #path and not subd:find'%W%W'
+			and (colon == '' or port ~= '' and port + 0 < 65536)
+			and (tlds[tld:lower()] or tld:find'^%d+$' and subd:find'^%d+%.%d+%.%d+%.$'
+			and max4(tld, subd:match'^(%d+)%.(%d+)%.(%d+)%.$') < 256)
+		then
+			finished[pos_start] = true
+			if #url >= minLength then
+				textNew = classicChat.insertlink(textNew, url);
+				classicChat.chatlog(url);
+				print(pos_start, url)
+			end
+		end
+	end
+
+	for pos_start, url, prot, dom, colon, port, slash, path in
+		text:gmatch'()((%f[%w]%a+://)(%w[-.%w]*)(:?)(%d*)(/?)([%w_.~!*:@&+$/?%%#=-]*))'
+	do
+		if not finished[pos_start] and not (dom..'.'):find'%W%W'
+			and protocols[prot:lower()] == (1 - #slash) * #path
+			and (colon == '' or port ~= '' and port + 0 < 65536)
+		then
+			if #url >= minLength then
+				textNew = classicChat.insertlink(textNew, url);
+				classicChat.chatlog(url);
+				print(pos_start, url)
+			end
+		end
+	end
+	return textNew;
+end
+
+function classicChat.insertlink(text, url)
+	local maxLength = 28;
+
+	text = classicChat.escape(text);
+	url = classicChat.escape(url);
+
+	local urlHttp = url;
+	if urlHttp:match("https?://") == nil then
+		urlHttp = "http://" .. urlHttp;
+	end
+
+	if #url >= maxLength then
+		text = text:gsub(url, string.format("{a SLL %s}{#%s}%s!@#DOT#@!!@#DOT#@!!@#DOT#@!{/}{/}{/}", urlHttp, settings.chatColors.Link, classicChat.escape(classicChat.unescape(url):sub(1, maxLength-3))));
+	else
+		text = text:gsub(url, string.format("{a SLL %s}{#%s}%s{/}{/}{/}", urlHttp, settings.chatColors.Link, url));
+	end
+
+	return classicChat.unescape(text);
+end
 
 function classicChat.escape(text)
 	text = text:gsub('[%%]','!@#PERCENT#@!');
@@ -485,8 +580,8 @@ function classicChat.chatlog(text)
 	else
 		lastMessage = text;
 		file:write(text.."\n");
-	    io.close(file);
-	    return true;
+		 io.close(file);
+		 return true;
 	end
 end
 
